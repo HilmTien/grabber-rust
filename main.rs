bb388310bc6d52471d204754350ef37aff343cd2
HilmTien <tient2477@gmail.com>
2024-04-08 23:11:07 +0200

    :sparkles: add CLI arguments

--- main.rs
+++ main.rs
@@ -2,21 +2,30 @@
 extern crate dotenv_codegen;
 
 use crate::models::MatchResponse;
+use anyhow::{anyhow, Result};
+use std::env;
 
 mod models;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        return Err(anyhow!("Missing command line argument mp_id"));
+    }
+
+    let mut mp_id: u32 = args[1].parse().unwrap();
+    let acronym = args[2].clone();
     let key = dotenv!("OSU_V1_API_KEY");
-    let mut mp_id = 0;
+
     loop {
         let url = format!("https://osu.ppy.sh/api/get_match?k={key}&mp={mp_id}");
         // let resp = reqwest::get(url).await?.text().await?;
         if let Ok(resp) = reqwest::get(url).await?.json::<MatchResponse>().await {
             // println!("{resp:#?}");
             let name = resp.match_.name;
-            println!("{name}");
-            if name.to_lowercase().contains("acronym") {
+            println!("{name}, {mp_id}");
+            if name.to_lowercase().contains(&acronym) {
                 println!("{}", mp_id);
                 break;
             }

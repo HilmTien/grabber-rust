1ab466279a8e27fa086a346d78a6e35a5a15d4c5
HilmTien <tient2477@gmail.com>
2024-04-08 22:32:22 +0200

    :sparkles: Added search algorithm

--- main.rs
+++ main.rs
@@ -8,10 +8,21 @@ mod models;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let key = dotenv!("OSU_V1_API_KEY");
-    let mp_id = "112312066";
-    let url = format!("https://osu.ppy.sh/api/get_match?k={key}&mp={mp_id}");
-    // let resp = reqwest::get(url).await?.text().await?;
-    let resp = reqwest::get(url).await?.json::<MatchResponse>().await?;
-    println!("{resp:#?}");
+    let mut mp_id = 0;
+    loop {
+        let url = format!("https://osu.ppy.sh/api/get_match?k={key}&mp={mp_id}");
+        // let resp = reqwest::get(url).await?.text().await?;
+        if let Ok(resp) = reqwest::get(url).await?.json::<MatchResponse>().await {
+            // println!("{resp:#?}");
+            let name = resp.match_.name;
+            println!("{name}");
+            if name.to_lowercase().contains("acronym") {
+                println!("{}", mp_id);
+                break;
+            }
+        }
+        mp_id += 1;
+    }
+
     Ok(())
 }
